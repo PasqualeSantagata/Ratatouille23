be20d76678e0659223f5de9e29829c6e0ef788cc
PasqualeSantagata <p.santagata@studenti.unina.it>
2023-10-07 17:47:31 +0200

    aggiornata schermata visualizzazione delle categoria

--- app/src/main/java/com/example/springclient/view/inserimentoNelMenu/VisualizzaElementiDellaCategoriaInserimentoNelMenuActivity.java
+++ app/src/main/java/com/example/springclient/view/inserimentoNelMenu/VisualizzaElementiDellaCategoriaInserimentoNelMenuActivity.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -26,7 +27,9 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.textfield.TextInputLayout;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class VisualizzaElementiDellaCategoriaInserimentoNelMenuActivity extends AppCompatActivity implements IRecycleViewElementoMenu {
 
@@ -41,6 +44,7 @@ public class VisualizzaElementiDellaCategoriaInserimentoNelMenuActivity extends 
     private int elementoSelezionato = -1;
     private ElementoMenuPresenter elementoMenuPresenter;
     private  RecycleViewAdapterGestioneElementoMenu adapterElementoMenu;
+    private ItemTouchHelper.SimpleCallback simpleCallback;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -52,6 +56,23 @@ public class VisualizzaElementiDellaCategoriaInserimentoNelMenuActivity extends 
         setContentView(R.layout.activity_visualizza_cagtegoria_inserimento_nel_menu);
         elementoMenuPresenter = new ElementoMenuPresenter(this);
 
+        simpleCallback = new ItemTouchHelper.SimpleCallback(ItemTouchHelper.UP | ItemTouchHelper.DOWN
+                | ItemTouchHelper.START | ItemTouchHelper.END, 0) {
+
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                int fromPosition = viewHolder.getAdapterPosition();
+                int toPosition = target.getAdapterPosition();
+                Collections.swap(elementiMenu, fromPosition, toPosition);
+                recyclerView.getAdapter().notifyItemMoved(fromPosition, toPosition);
+                return false;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+
+            }
+        };
 
         initializeComponents();
 
@@ -92,37 +113,20 @@ public class VisualizzaElementiDellaCategoriaInserimentoNelMenuActivity extends 
 
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         recyclerViewPiatti.setLayoutManager(linearLayoutManager);
+        recyclerViewPiatti.setItemAnimator(null);
 
         //Dovrebbe implementare la funzione drug and drop
-        //DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
-        //recyclerViewPiatti.addItemDecoration(dividerItemDecoration);
-        //ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleCallback);
-        //itemTouchHelper.attachToRecyclerView(recyclerViewPiatti);
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        recyclerViewPiatti.addItemDecoration(dividerItemDecoration);
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleCallback);
+        itemTouchHelper.attachToRecyclerView(recyclerViewPiatti);
 
     }
-
-   /* ItemTouchHelper.SimpleCallback simpleCallback = new ItemTouchHelper.SimpleCallback(ItemTouchHelper.UP | ItemTouchHelper.DOWN | ItemTouchHelper.START | ItemTouchHelper.END, 0) {
-
-        @Override
-        public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
-
-            int fromPosition = viewHolder.getAdapterPosition();
-            int toPosition = target.getAdapterPosition();
-            Collections.swap(elementiMenu, fromPosition, toPosition);
-            recyclerView.getAdapter().notifyItemMoved(fromPosition, toPosition);
-            return false;
-        }
-
-        @Override
-        public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
-
-        }
-    };*/
 
     public void setParameters(ElementoMenu elementoMenu){
         setTextInputLayoutText(textInputLayoutPrezzo, String.valueOf(elementoMenu.getPrezzo()));
         //Attenzione controllare il to string default di List
-        setTextInputLayoutText(textInputLayoutAllergeni, elementoMenu.getElencoAllergeni().toString());
+        //setTextInputLayoutText(textInputLayoutAllergeni, elementoMenu.getElencoAllergeni().toString());
         setTextInputLayoutText(textInputLayoutDescrizione, elementoMenu.getDescrizione());
     }
 
